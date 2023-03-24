81437629a93fb3eb044b353ba1183140a14427d3
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-03-24 06:23:52 -0700

    Dropdown Menu & Sort Gigs

--- client/src/pages/gigs/Gigs.jsx
+++ client/src/pages/gigs/Gigs.jsx
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Gigs.scss";
 
 const Gigs = () => {
-  return <div>Gigs</div>;
+  const [sort, setSort] = useState("sales");
+  const [open, setOpen] = useState(false);
+
+  const reSort = (type) => {
+    setSort(type);
+    setOpen(false);
+  };
+
+  return (
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">HOBBYJOBBY > GRAPHICS & DESIGN ></span>
+        <h1>AI Artists</h1>
+        <p>
+          Explore the boundaries of art and technology with HobbyJobby's AI
+          artists
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budget</span>
+            <input type="text" placeholder="min" />
+            <input type="text" placeholder="max" />
+            <button>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sortBy">Sort By</span>
+            <span className="sortType">
+              {sort === "sales" ? "Best Selling" : "Newest"}
+            </span>
+            <img src="./img/down.png" alt="" onClick={() => setOpen(!open)} />
+            {open && (
+              <div className="rightMenu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Gigs;

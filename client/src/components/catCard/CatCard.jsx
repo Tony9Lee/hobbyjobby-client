0a8d3398c27a31958eb800271e50dcb4e61add98
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-03-24 02:59:29 -0700

    Project Card / 2nd Carousel

--- client/src/components/catCard/CatCard.jsx
+++ client/src/components/catCard/CatCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Catcard.scss";
+import "./CatCard.scss";
 
 const CatCard = ({ item }) => {
   return (
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import Slide from "../../components/Slide/Slide";
-import { cards } from "../../data";
+import { cards, projects } from "../../data";
 import "./Home.scss";
 import CatCard from "../../components/catCard/CatCard";
+import ProjectCard from "../../components/projectCard/ProjectCard";
 
 const Home = () => {
   return (
@@ -58,6 +59,44 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>
+              HobbyJobby <i>Business</i>
+            </h1>
+            <h1>A Business Solution Designed for Teams</h1>
+            <p>
+              Upgrade to a curated experience packed with tools and benefits,
+              dedicated to businesses
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven business experience
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Get matched with the perfect talent by a customer success manager
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Manage teamwork and boost productivity with one powerful workspace
+            </div>
+            <button>Explore HobbyJobby Business</button>
+          </div>
+          <div className="item">
+            <img
+              src="https://fiverr-res.cloudinary.com/q_auto,f_auto,w_870,dpr_1.0/v1/attachments/generic_asset/asset/d9c17ceebda44764b591a8074a898e63-1599597624757/business-desktop-870-x1.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map((card) => (
+          <ProjectCard key={card.id} item={card} />
+        ))}
+      </Slide>
     </div>
   );
 };

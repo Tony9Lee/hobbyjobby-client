5e584ccea32b513d1eb2e539bdf90b3d3231eb0c
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-03-29 09:42:35 -0700

    Login and Formatting

---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import axios from "axios";
+import "./Login.scss";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("localhost:8800/api/auth/login", {
+        username,
+        password,
+      });
+      console.log(res.data);
+    } catch (error) {
+      setError(err);
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          name="username"
+          type="text"
+          placeholder="johndoe"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="">Username</label>
+        <input
+          name="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;

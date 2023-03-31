82f7611555bf5b6a3a7fd586cda8420683733f78
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-03-31 16:41:27 -0700

    Modified App.jsx & ENV file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,20 +4,18 @@ import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Gigs from "./pages/gigs/Gigs";
 import Gig from "./pages/gig/Gig";
-import Add from "./pages/add/Add";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Add from "./pages/add/Add";
 import Orders from "./pages/orders/Orders";
 import Messages from "./pages/messages/Messages";
 import Message from "./pages/message/Message";
 import MyGigs from "./pages/myGigs/MyGigs";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
-import "./App.scss";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import "./app.scss";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Pay from "./pages/pay/Pay";
+import Success from "./pages/success/Success";
 
 function App() {
   const queryClient = new QueryClient();
@@ -48,20 +46,12 @@ function App() {
           element: <Gigs />,
         },
         {
-          path: "/gig/:id",
-          element: <Gig />,
+          path: "/myGigs",
+          element: <MyGigs />,
         },
         {
           path: "/orders",
           element: <Orders />,
-        },
-        {
-          path: "/mygigs",
-          element: <MyGigs />,
-        },
-        {
-          path: "/add",
-          element: <Add />,
         },
         {
           path: "/messages",
@@ -72,6 +62,14 @@ function App() {
           element: <Message />,
         },
         {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+        {
           path: "/register",
           element: <Register />,
         },
@@ -79,16 +77,19 @@ function App() {
           path: "/login",
           element: <Login />,
         },
-        ,
+        {
+          path: "/pay/:id",
+          element: <Pay />,
+        },
+        {
+          path: "/success",
+          element: <Success />,
+        },
       ],
     },
   ]);
 
-  return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;

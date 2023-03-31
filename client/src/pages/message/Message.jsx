853de05a94bcb1de6b3393a861c5faef1c5d9d7b
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-03-31 03:22:30 -0700

    Messages Setup with ID

--- client/src/pages/message/Message.jsx
+++ client/src/pages/message/Message.jsx
@@ -1,122 +1,65 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import newRequest from "../../utils/newRequest";
 import "./Message.scss";
 
 const Message = () => {
+  const { id } = useParams();
+
+  const queryClient = useQueryClient();
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["messages"],
+    queryFn: () =>
+      newRequest.get(`/messages/${id}`).then((res) => {
+        return res.data;
+      }),
+  });
+
+  const mutation = useMutation({
+    mutationFn: (message) => {
+      return newRequest.post(`/messages`, message);
+    },
+    onSuccess: () => {
+      QueryClient.invalidateQueries(["messages"]);
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    mutation.mutate({
+      conversationId: id,
+      desc: e.target[0].value,
+    });
+    e.target[0].value = "";
+  };
+
   return (
     <div className="message">
       <div className="container">
         <span className="breadcrumbs">
           <Link to="/messages">MESSAGES | JOHN DOE </Link>
         </span>
-        <div className="messages">
-          <div className="item">
-            <img
-              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Accusamus doloribus mollitia, modi totam nobis quisquam
-              perspiciatis inventore incidunt, officiis maiores iste
-              necessitatibus quod quis eius suscipit velit quas ipsum!
-              Provident?
-            </p>
+        {isLoading ? (
+          "loading"
+        ) : error ? (
+          "error"
+        ) : (
+          <div className="messages">
+            {data.map((m) => (
+              <div className="item" key={m._id}>
+                <img
+                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt=""
+                />
+                <p>{m.desc}</p>
+              </div>
+            ))}
           </div>
-          <div className="item owner">
-            <img
-              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Accusamus doloribus mollitia, modi totam nobis quisquam
-              perspiciatis inventore incidunt, officiis maiores iste
-              necessitatibus quod quis eius suscipit velit quas ipsum!
-              Provident?
-            </p>
-          </div>
-          <div className="item">
-            <img
-              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Accusamus doloribus mollitia, modi totam nobis quisquam
-              perspiciatis inventore incidunt, officiis maiores iste
-              necessitatibus quod quis eius suscipit velit quas ipsum!
-              Provident?
-            </p>
-          </div>
-          <div className="item owner">
-            <img
-              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Accusamus doloribus mollitia, modi totam nobis quisquam
-              perspiciatis inventore incidunt, officiis maiores iste
-              necessitatibus quod quis eius suscipit velit quas ipsum!
-              Provident?
-            </p>
-          </div>
-          <div className="item">
-            <img
-              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Accusamus doloribus mollitia, modi totam nobis quisquam
-              perspiciatis inventore incidunt, officiis maiores iste
-              necessitatibus quod quis eius suscipit velit quas ipsum!
-              Provident?
-            </p>
-          </div>
-          <div className="item owner">
-            <img
-              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Accusamus doloribus mollitia, modi totam nobis quisquam
-              perspiciatis inventore incidunt, officiis maiores iste
-              necessitatibus quod quis eius suscipit velit quas ipsum!
-              Provident?
-            </p>
-          </div>
-          <div className="item">
-            <img
-              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Accusamus doloribus mollitia, modi totam nobis quisquam
-              perspiciatis inventore incidunt, officiis maiores iste
-              necessitatibus quod quis eius suscipit velit quas ipsum!
-              Provident?
-            </p>
-          </div>
-          <div className="item owner">
-            <img
-              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Accusamus doloribus mollitia, modi totam nobis quisquam
-              perspiciatis inventore incidunt, officiis maiores iste
-              necessitatibus quod quis eius suscipit velit quas ipsum!
-              Provident?
-            </p>
-          </div>
-        </div>
+        )}
         <hr />
-        <div className="write">
+        <form className="write" onSubmit={handleSubmit}>
           <textarea
             name=""
             placeholder="write a message"
@@ -124,8 +67,8 @@ const Message = () => {
             cols="30"
             rows="10"
           ></textarea>
-          <button>Send</button>
-        </div>
+          <button type="submit">Send</button>
+        </form>
       </div>
     </div>
   );
--- client/src/pages/messages/Messages.jsx
+++ client/src/pages/messages/Messages.jsx
@@ -1,108 +1,87 @@
 import React from "react";
-import "./Messages.scss";
 import { Link } from "react-router-dom";
+import {
+  QueryClient,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
+import "./Messages.scss";
+import moment from "moment";
 
 const Messages = () => {
-  const currentUser = {
-    id: 1,
-    username: "John Doe",
-    isSeller: true,
-  };
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
-  const message =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, animi eaque, est officia blanditiis explicabo culpa, ut doloribus amet itaque quas excepturi non! Ratione, quam voluptate sed eaque tempore cupiditate.";
+  const queryClient = useQueryClient();
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["conversations"],
+    queryFn: () =>
+      newRequest.get(`/conversations`).then((res) => {
+        return res.data;
+      }),
+  });
+
+  const mutation = useMutation({
+    mutationFn: (id) => {
+      return newRequest.put(`/conversations/${id}`);
+    },
+    onSuccess: () => {
+      QueryClient.invalidateQueries(["conversations"]);
+    },
+  });
+
+  const handleRead = (id) => {
+    mutation.mutate(id);
+  };
 
   return (
     <div className="messages">
-      <div className="container">
-        <div className="title">
-          <h1>Messages</h1>
+      {isLoading ? (
+        "loading"
+      ) : error ? (
+        "error"
+      ) : (
+        <div className="container">
+          <div className="title">
+            <h1>Messages</h1>
+          </div>
+          <table>
+            <tr>
+              <th>{currentUser.isSeller ? "Buyer" : "Seller"}</th>
+              <th>Last Message</th>
+              <th>Date</th>
+              <th>Action</th>
+            </tr>
+            {data.map((c) => (
+              <tr
+                className={
+                  ((currentUser.isSeller && !c.readBySeller) ||
+                    (!currentUser.isSeller && !c.readByBuyer)) &&
+                  "active"
+                }
+                key={c.id}
+              >
+                <td>{currentUser.isSeller ? c.buyerId : c.sellerId}</td>
+                <td>
+                  <Link to={`/message/${c.id}`} className="link">
+                    {c?.lastMessage?.substring(0, 100)}...
+                  </Link>
+                </td>
+                <td>{moment(c.updatedAt).fromNow()}</td>
+                <td>
+                  {((currentUser.isSeller && !c.readBySeller) ||
+                    (!currentUser.isSeller && !c.readByBuyer)) && (
+                    <button onClick={() => handleRead(c.id)}>
+                      Mark As Read
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </table>
         </div>
-        <table>
-          <tr>
-            <th>Buyer</th>
-            <th>Last Message</th>
-            <th>Date</th>
-            <th>Action</th>
-          </tr>
-          <tr className="active">
-            <td>John Doe</td>
-            <td>
-              <Link to="/message/1" className="link">
-                {message.substring(0, 100)}
-              </Link>
-              ...
-            </td>
-            <td>1 Day Ago</td>
-            <td>
-              <button>Mark As Read</button>
-            </td>
-          </tr>
-          <tr className="active">
-            <td>John Doe</td>
-            <td>
-              <Link to="/message/1" className="link">
-                {message.substring(0, 100)}
-              </Link>
-              ...
-            </td>
-            <td>1 Day Ago</td>
-            <td>
-              <button>Mark As Read</button>
-            </td>
-          </tr>
-          <tr>
-            <td>John Doe</td>
-            <td>
-              <Link to="/message/1" className="link">
-                {message.substring(0, 100)}
-              </Link>
-              ...
-            </td>
-            <td>1 Day Ago</td>
-          </tr>
-          <tr>
-            <td>John Doe</td>
-            <td>
-              <Link to="/message/1" className="link">
-                {message.substring(0, 100)}
-              </Link>
-              ...
-            </td>
-            <td>1 Day Ago</td>
-          </tr>
-          <tr>
-            <td>John Doe</td>
-            <td>
-              <Link to="/message/1" className="link">
-                {message.substring(0, 100)}
-              </Link>
-              ...
-            </td>
-            <td>1 Day Ago</td>
-          </tr>
-          <tr>
-            <td>John Doe</td>
-            <td>
-              <Link to="/message/1" className="link">
-                {message.substring(0, 100)}
-              </Link>
-              ...
-            </td>
-            <td>1 Day Ago</td>
-          </tr>
-          <tr>
-            <td>John Doe</td>
-            <td>
-              <Link to="/message/1" className="link">
-                {message.substring(0, 100)}
-              </Link>
-              ...
-            </td>
-            <td>1 Day Ago</td>
-          </tr>
-        </table>
-      </div>
+      )}
     </div>
   );
 };

314d6f006420dedd7ce8c0a8de3a69d5743f9e1e
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-04-01 03:14:09 -0700

    Success Payment Page to Redirect

---
+++ client/src/pages/success/Success.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import newRequest from "../../utils/newRequest";
+
+const Success = () => {
+  const { search } = useLocation(); // useLocation hook to be used to
+  const navigate = useNavigate(); //To be redirected after order is updated using useNavigate hook
+  const params = new URLSearchParams(search);
+  const payment_intent = params.get("payment_intent");
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        await newRequest.put("/orders", { payment_intent });
+        setTimeout(() => {
+          navigate("/orders");
+        }, 5000);
+      } catch (error) {}
+    };
+    makeRequest();
+  }, []);
+
+  return (
+    <div>
+      Payment successful. Please do not close this page as you are being
+      redirected to your order.
+    </div>
+  );
+};
+
+export default Success;

66175dc5d8ae8eae457da216fa35215b0c9dce8c
tony l <33114223+Tony9Lee@users.noreply.github.com>
2023-04-15 00:48:59 -0700

    Client

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,34 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Featured.scss";
+import { useNavigate } from "react-router-dom";
 
-const Featured = () => {
+function Featured() {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    navigate(`/gigs?search=${input}`);
+  };
   return (
     <div className="featured">
       <div className="container">
         <div className="left">
           <h1>
-            Find the Perfect Person to <i>freelance</i> Business Services
+            Find the perfect <i>freelance</i> services for your business
           </h1>
           <div className="search">
             <div className="searchInput">
               <img src="./img/search.png" alt="" />
-              <input type="text" placeholder='Try "Building a Mobile App!"' />
+              <input
+                type="text"
+                placeholder='Try "building mobil app"'
+                onChange={(e) => setInput(e.target.value)}
+              />
             </div>
-            <button>Search</button>
+            <button onClick={handleSubmit}>Search</button>
           </div>
           <div className="popular">
-            <span>Popular</span>
+            <span>Popular:</span>
             <button>Web Design</button>
-            <button>Wordpress</button>
+            <button>WordPress</button>
             <button>Logo Design</button>
             <button>AI Services</button>
           </div>
         </div>
-        <div className="right"></div>
-        <img src="./img/man.png" alt="" />
+        <div className="right">
+          <img src="./img/man.png" alt="" />
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default Featured;
